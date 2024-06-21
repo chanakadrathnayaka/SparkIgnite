2ac2fc1a894f72815ed15ad0ad527f15b94bd237
chanaka <chanakadrathnayaka@gmail.com>
2024-06-21 16:57:34 -0700

    There is an error with SLF4J

---
+++ src/main/java/com/iamchanaka/sparkignitetest/SparkIgniteTestApplication.java
@@ -0,0 +1,13 @@
+package com.iamchanaka.sparkignitetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkIgniteTestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SparkIgniteTestApplication.class, args);
+  }
+
+}
